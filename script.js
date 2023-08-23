6baa03397908e25756eebad217e94456ee169820
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-08-23 17:43:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let pro=new Promise((resolve, reject)=>{
+	setTimeout(
+	 resolve("Hello,world!"),1000);
+	
+});
+prom.then((data)=>{
+	const ele =document.getElementById("output");
+	ele.innerText=data;
+})
